1ec1e7a5d86010ea815d6d1c5cef1acc23af66be
Vitold55 <vitold55@ukr.net>
2019-11-15 23:56:33 +0200

    Modules. Variant of import, custom module

---
+++ custModule.py
@@ -0,0 +1,8 @@
+def get_len(iObj):
+    cnt = 0
+    for i in iObj:
+        cnt += 1
+    return cnt
+
+if __name__ == '__main':
+    print('Execute only in this file, not in imported')
---
+++ lesson30.py
@@ -0,0 +1,9 @@
+import os
+import random as rand
+from random import randint
+
+import custModule
+
+print(randint(1, 30))
+
+print(custModule.get_len('hello'))
