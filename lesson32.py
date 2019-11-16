d09d2509366563f4ad0689d6aa54405b75fe98d6
Vitold55 <vitold55@ukr.net>
2019-11-16 13:44:58 +0200

    Def function for showing folder tree

--- lesson32.py
+++ lesson32.py
@@ -1,13 +1,17 @@
 import os
 
-tree = os.walk('folder')
+def get_folder_tree(basefolder):
+    tree = os.walk(basefolder)
 
-for root, subfolders, files in tree:
-    level = root.count(os.sep)
-    indent = ' ' * 4 * level
-    sub_indent = ' ' * 4 * (level + 1)
+    for root, subfolders, files in tree:
+        level = root.count(os.sep)
+        indent = ' ' * 4 * level
+        sub_indent = ' ' * 4 * (level + 1)
 
-    print(indent + os.path.basename(root))
+        print(indent + os.path.basename(root))
 
-    for file in files:
-        print(sub_indent + file)
+        for file in files:
+            print(sub_indent + file)
+
+
+get_folder_tree('folder')
