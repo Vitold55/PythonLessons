29bf4fff1e1825268622696e1d04f52ff2f7af95
Vitold55 <vitold55@ukr.net>
2019-11-16 11:53:21 +0200

    Second variant of Guess number game

--- guessNumberGame.py
+++ guessNumberGame.py
@@ -1,27 +1,51 @@
 from random import randint
 import sys
 
-def guessNumberGame():
-    number = randint(1, 3)
-    counter = 0
+number = randint(1, 100)
+counter = 0
 
-    while True:
+while True:
 
-        attempt = int(input('Input your number variant? '))
-        counter += 1
+    attempt = int(input('Input your number variant? '))
+    counter += 1
 
-        if attempt == number:
-            print(f'Congratulation!!! You guess the number by {counter} attempts')
-            playAgain = input('If you want to play one more time, please, type Y! ')
+    if attempt == number:
+        print(f'Congratulation!!! You guess the number by {counter} attempts')
+        playAgain = input('If you want to play one more time, please, type Y! ')
 
-            if playAgain == 'Y':
-                guessNumberGame()
-            else:
-                sys.exit()
-        elif attempt < number:
-            print('Input bigger number')
+        if playAgain == 'Y':
+            number = randint(1, 3)
+            counter = 0
         else:
-            print('Input less number')
+            print('Thanks for playing this game!')
+            break
+    elif attempt < number:
+        print('Input bigger number')
+    else:
+        print('Input less number')
+
+# Second variant
+# def guessNumberGame():
+#     number = randint(1, 100)
+#     counter = 0
+
+#     while True:
+
+#         attempt = int(input('Input your number variant? '))
+#         counter += 1
+
+#         if attempt == number:
+#             print(f'Congratulation!!! You guess the number by {counter} attempts')
+#             playAgain = input('If you want to play one more time, please, type Y! ')
+
+#             if playAgain == 'Y':
+#                 guessNumberGame()
+#             else:
+#                 sys.exit()
+#         elif attempt < number:
+#             print('Input bigger number')
+#         else:
+#             print('Input less number')
 
 
-guessNumberGame()
+# guessNumberGame()
